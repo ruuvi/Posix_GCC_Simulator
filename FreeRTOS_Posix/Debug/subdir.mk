8fe9c17322013dcd22a4609265fb53055ced4ef3
arkady <larytet@yahoo.com>
2015-05-16 12:58:38 +0300

    Rename main.c is CPP file

--- FreeRTOS_Posix/Debug/subdir.mk
+++ FreeRTOS_Posix/Debug/subdir.mk
@@ -4,7 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../main.c 
+../main.cpp
 
 OBJS += \
 ./main.o 
@@ -21,4 +21,12 @@ C_DEPS += \
 	@echo 'Finished building: $<'
 	@echo ' '
 
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -D__GCC_POSIX__=1 -DDEBUG_BUILD=1 -DUSE_STDIO=1 -I../Common_Demo/include -I.. -I../FreeRTOS_Kernel/include -I../FreeRTOS_Kernel/portable/GCC/Posix -O0 -g -Wall -c -fmessage-length=0 -pthread -Wno-pointer-sign -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<" -lrt
+	@echo 'Finished building: $<'
+	@echo ' '
 
