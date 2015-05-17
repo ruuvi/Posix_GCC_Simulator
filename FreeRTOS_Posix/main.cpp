2ba6aaf9607325e1858e5bf69000e69fa82691ac
arkady <larytet@yahoo.com>
2015-05-17 07:09:09 +0300

    JobThread template non static

--- FreeRTOS_Posix/main.cpp
+++ FreeRTOS_Posix/main.cpp
@@ -238,7 +238,8 @@ template<typename JobType>
 JobThread<JobType>::JobThread() : job(nullptr) {
     static const char *name = "a job";
     vSemaphoreCreateBinary(this->signal);
-    portBASE_TYPE res = xTaskCreate((pdTASK_CODE)mainLoop, (const signed char *)name, 300, this, 1, &this->pxCreatedTask);
+    void *pMainLoop = (void*)&JobThread<JobType>::mainLoop;
+    portBASE_TYPE res = xTaskCreate((pdTASK_CODE)pMainLoop, (const signed char *)name, 300, this, 1, &this->pxCreatedTask);
     if (res != pdPASS) {
         cout << "Failed to create a task" << endl;
     }
