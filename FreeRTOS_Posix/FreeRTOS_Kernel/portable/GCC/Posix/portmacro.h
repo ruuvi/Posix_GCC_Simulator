26c63b322b96a7215993e1920313fd43948ba95a
Miroslav Bendík <miroslav.bendik@gmail.com>
2019-12-21 15:20:29 +0100

    Updated FreeRTOS kernel

--- FreeRTOS_Posix/FreeRTOS_Kernel/portable/GCC/Posix/portmacro.h
+++ FreeRTOS_Posix/FreeRTOS_Kernel/portable/GCC/Posix/portmacro.h
@@ -1,130 +1,71 @@
 /*
-	FreeRTOS.org V5.2.0 - Copyright (C) 2003-2009 Richard Barry.
-
-	This file is part of the FreeRTOS.org distribution.
-
-	FreeRTOS.org is free software; you can redistribute it and/or modify it
-	under the terms of the GNU General Public License (version 2) as published
-	by the Free Software Foundation and modified by the FreeRTOS exception.
-
-	FreeRTOS.org is distributed in the hope that it will be useful,	but WITHOUT
-	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-	FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-	more details.
-
-	You should have received a copy of the GNU General Public License along
-	with FreeRTOS.org; if not, write to the Free Software Foundation, Inc., 59
-	Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-
-	A special exception to the GPL is included to allow you to distribute a
-	combined work that includes FreeRTOS.org without being obliged to provide
-	the source code for any proprietary components.  See the licensing section
-	of http://www.FreeRTOS.org for full details.
-
-
-	***************************************************************************
-	*                                                                         *
-	* Get the FreeRTOS eBook!  See http://www.FreeRTOS.org/Documentation      *
-	*                                                                         *
-	* This is a concise, step by step, 'hands on' guide that describes both   *
-	* general multitasking concepts and FreeRTOS specifics. It presents and   *
-	* explains numerous examples that are written using the FreeRTOS API.     *
-	* Full source code for all the examples is provided in an accompanying    *
-	* .zip file.                                                              *
-	*                                                                         *
-	***************************************************************************
-
-	1 tab == 4 spaces!
-
-	Please ensure to read the configuration and relevant port sections of the
-	online documentation.
-
-	http://www.FreeRTOS.org - Documentation, latest information, license and
-	contact details.
-
-	http://www.SafeRTOS.com - A version that is certified for use in safety
-	critical systems.
-
-	http://www.OpenRTOS.com - Commercial support, development, porting,
-	licensing and training services.
-*/
+ * FreeRTOS Kernel V10.2.1
+ */
 
 
 #ifndef PORTMACRO_H
 #define PORTMACRO_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/*-----------------------------------------------------------
- * Port specific definitions.
- *
- * The settings in this file configure FreeRTOS correctly for the
- * given hardware and compiler.
- *
- * These settings should not be altered.
- *-----------------------------------------------------------
- */
 
 /* Type definitions. */
-#define portCHAR		char
-#define portFLOAT		float
-#define portDOUBLE		double
-#define portLONG		int
-#define portSHORT		short
-#define portSTACK_TYPE  unsigned long
-#define portBASE_TYPE   long
+#define portSTACK_TYPE unsigned long
+#define portBASE_TYPE long
+#define portLONG long
+#define portSHORT short
+#define portUBASE_TYPE unsigned long
+#define portMAX_DELAY  (TickType_t) 0xffffffffffffffffUL
+#define portPOINTER_SIZE_TYPE uint64_t
 
-#if( configUSE_16_BIT_TICKS == 1 )
-	typedef unsigned portSHORT portTickType;
-	#define portMAX_DELAY ( portTickType ) 0xffff
-#else
-	typedef unsigned portLONG portTickType;
-	#define portMAX_DELAY ( portTickType ) 0xffffffff
-#endif
-/*-----------------------------------------------------------*/
+typedef portSTACK_TYPE StackType_t;
+typedef portBASE_TYPE BaseType_t;
+typedef portUBASE_TYPE UBaseType_t;
+typedef portUBASE_TYPE TickType_t;
 
 /* Architecture specifics. */
-#define portSTACK_GROWTH				( -1 )
-#define portTICK_RATE_MS				( ( portTickType ) 1000 / configTICK_RATE_HZ )
-#define portTICK_RATE_MICROSECONDS		( ( portTickType ) 1000000 / configTICK_RATE_HZ )
-#define portBYTE_ALIGNMENT				4
-#define portREMOVE_STATIC_QUALIFIER
-/*-----------------------------------------------------------*/
-
+#define portSTACK_GROWTH ( -1 )
+#define portTICK_PERIOD_MS ( ( TickType_t ) 1000 / configTICK_RATE_HZ )
+#define portTICK_RATE_MICROSECONDS ( ( TickType_t ) 1000000 / configTICK_RATE_HZ )
+#define portBYTE_ALIGNMENT 8
 
 /* Scheduler utilities. */
 extern void vPortYieldFromISR( void );
 extern void vPortYield( void );
-
-#define portYIELD()					vPortYield()
-
+extern void vTaskSwitchContext( void );
+#define portYIELD() vPortYield()
 #define portEND_SWITCHING_ISR( xSwitchRequired ) if( xSwitchRequired ) vPortYieldFromISR()
-/*-----------------------------------------------------------*/
+#define portYIELD_FROM_ISR( x ) portEND_SWITCHING_ISR( x )
 
 
 /* Critical section management. */
 extern void vPortDisableInterrupts( void );
 extern void vPortEnableInterrupts( void );
-#define portSET_INTERRUPT_MASK()	( vPortDisableInterrupts() )
-#define portCLEAR_INTERRUPT_MASK()	( vPortEnableInterrupts() )
+#define portSET_INTERRUPT_MASK()( vPortDisableInterrupts() )
+#define portCLEAR_INTERRUPT_MASK()( vPortEnableInterrupts() )
 
 extern portBASE_TYPE xPortSetInterruptMask( void );
 extern void vPortClearInterruptMask( portBASE_TYPE xMask );
 
-#define portSET_INTERRUPT_MASK_FROM_ISR()		xPortSetInterruptMask()
-#define portCLEAR_INTERRUPT_MASK_FROM_ISR(x)	vPortClearInterruptMask(x)
+#define portSET_INTERRUPT_MASK_FROM_ISR() xPortSetInterruptMask()
+#define portCLEAR_INTERRUPT_MASK_FROM_ISR(x) vPortClearInterruptMask(x)
 
+#define portCRITICAL_NESTING_IN_TCB 1
+extern void vTaskEnterCritical( void );
+extern void vTaskExitCritical( void );
 
 extern void vPortEnterCritical( void );
 extern void vPortExitCritical( void );
 
-#define portDISABLE_INTERRUPTS()	portSET_INTERRUPT_MASK()
-#define portENABLE_INTERRUPTS()		portCLEAR_INTERRUPT_MASK()
-#define portENTER_CRITICAL()		vPortEnterCritical()
-#define portEXIT_CRITICAL()			vPortExitCritical()
-/*-----------------------------------------------------------*/
+#define portDISABLE_INTERRUPTS() portSET_INTERRUPT_MASK()
+#define portENABLE_INTERRUPTS() portCLEAR_INTERRUPT_MASK()
+#define portENTER_CRITICAL() vTaskEnterCritical()
+#define portEXIT_CRITICAL() vTaskExitCritical()
+
 
 /* Task function macros as described on the FreeRTOS.org WEB site. */
 #define portTASK_FUNCTION_PROTO( vFunction, pvParameters ) void vFunction( void *pvParameters )
@@ -159,6 +100,8 @@ extern void vPortFindTicksPerSecond( void );
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS()	vPortFindTicksPerSecond()		/* Nothing to do because the timer is already present. */
 extern unsigned long ulPortGetTimerValue( void );
 #define portGET_RUN_TIME_COUNTER_VALUE()			ulPortGetTimerValue()			/* Query the System time stats for this process. */
+
+#define xTaskCreatePinnedToCore(code, name, stack, parameters, priority, handle, core) xTaskCreate(code, name, stack, parameters, priority, handle)
 
 #ifdef __cplusplus
 }
